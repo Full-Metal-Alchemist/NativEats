2613d094875b8ddf88174d31f124e8480b0edcbc
Blake Hansen <110576621+blake-Hansen@users.noreply.github.com>
2023-06-10 15:47:24 -0700

    File structure and Navigation (#3)
    * File structure and navigation
    
    * Removed src directory and moved screens to app level

---
+++ app/screens/authentication/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+  touchable: {
+    width: 300,
+    marginTop: 556,
+    marginLeft: 25,
+    paddingVertical: 18,
+    borderWidth: 2,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+function Login(props) {
+  console.log(props.navigation)
+  return (
+    <View>
+      <Text style={styles.text}>This is the home screen!</Text>
+      <Button
+        onPress={() => props.navigation.navigate('SignupScreen')}
+        title="Sign Up"
+      />
+      <TouchableOpacity
+        style={styles.touchable}
+        onPress={() => props.navigation.navigate('ForgotPasswordScreen')}>
+        <Text>Forgot Password</Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
+
+
+export default Login;
