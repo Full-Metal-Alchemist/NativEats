f8accb45c7ed577988c731a13cba2dd7b1f811f3
mellava <6126925+mellava@users.noreply.github.com>
2023-06-10 15:52:52 -0700

    Create constants file

---
+++ app/constants/colors.js
@@ -0,0 +1,8 @@
+// Names from https://www.color-name.com/
+export const COLORS = {
+    TURQUOISE: "#55d6bf",
+    ORANGE: "#ef583c",
+    SCARLET: "#520028",
+    JASMINE: "#ffdf7a",
+    LEMON: "#fff5c9",
+};
