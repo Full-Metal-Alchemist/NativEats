37cd3ef278a2ddd7fb0342fe7818d6e97ad0d0c5
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-11 10:09:17 -0700

    swapped navigator for updated structure (#9)

--- app/screens/authentication/ForgotPass.jsx
+++ app/screens/authentication/ForgotPass.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import {
+  View, Text, StyleSheet, FlatList,
+} from 'react-native';
 
 function ForgotPassword() {
-
   return (
     <View>
       <Text>Dude wheres my car?</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   textStyle: {
     marginVertical: 50,
-  }
+  },
 });
 
 export default ForgotPassword;
