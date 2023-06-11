99a613b544bc08bb948e7d7a928eb71668d16112
Ruojia Liu <rul008@ucsd.edu>
2023-06-11 14:04:07 -0700

    change

--- app/App.jsx
+++ app/App.jsx
@@ -1,25 +1,31 @@
 import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Login from './screens/authentication/Login';
-import Signup from './screens/authentication/Signup';
-import ForgotPassword from './screens/authentication/ForgotPass';
+// import Login from './screens/authentication/Login';
+// import Signup from './screens/authentication/Signup';
+// import ForgotPassword from './screens/authentication/ForgotPass';
+// import RecipesMain from './screens/recipes/RecipesMainScreen';
+// import RecipesSingle from './screens/recipes/RecipesSingleScreen';
+import Login from './screens/authentication/Login'
 
-const navigator = createStackNavigator(
-  {
-    HomeScreen: Login,
-    SignupScreen: Signup,
-    ForgotPasswordScreen: ForgotPassword,
-  },
-  {
-    initialRouteName: 'HomeScreen',
-    defaultNavigationOptions: {
-      title: 'App',
-    },
-  },
-);
+const Stack = createStackNavigator();
 
-export default createAppContainer(navigator);
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {/* <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="RecipesMain" component={RecipesMain} />
+        <Stack.Screen name="RecipesSingle" component={RecipesSingle} /> */}
+        <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="BookMark" component={BookMark} />
+        <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
