776fac7292e091287c60e2f493826ab4e05bbc83
mellava <6126925+mellava@users.noreply.github.com>
2023-06-11 11:09:46 -0700

    Modify mock cuisines data based on wireframe

--- __mocks__/mockCuisines.js
+++ __mocks__/mockCuisines.js
@@ -3,10 +3,12 @@ module.exports = [
     id: '1',
     name: 'Japanese',
     about: 'Something about Japanese',
+    photo: 'https://images.unsplash.com/photo-1580822184713-fc5400e7fe10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
   },
   {
     id: '2',
     name: 'Korean',
     about: 'Something about Korean',
+    photo: 'https://images.unsplash.com/photo-1590301157890-4810ed352733?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=436&q=80',
   },
 ];
