966cd6ff61676950a2f501a23f66a4df08045313
Blake Hansen <b.hansen9264@gmail.com>
2023-06-11 10:36:03 -0700

    Modified two files for Recipe Screens and added routing in app.jsx file for those screens

---
+++ app/screens/recipes/RecipesSingleScreen.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  Text, StyleSheet, View, Button, TouchableOpacity,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+  touchable: {
+    width: 300,
+    marginTop: 556,
+    marginLeft: 25,
+    paddingVertical: 18,
+    borderWidth: 2,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+function RecipeSingle({ navigation }) {
+  return (
+    <View>
+      <Text style={styles.text}>This is the Signle Recipe screen!</Text>
+    </View>
+  );
+}
+
+export default RecipeSingle;
