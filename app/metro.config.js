6ceb04a73745c1224c4d97b6345cf7a16bffe294
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-13 08:07:18 -0700

    reverted metro.config to config recommended by using firebase expo docs

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,21 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
-const workspaceRoot = __dirname;
-const projectRoot = path.resolve(workspaceRoot, '../');
-console.log('Project and Workspace roots:', projectRoot, workspaceRoot);
-
-const defaultConfig = getDefaultConfig(projectRoot);
-
-defaultConfig.watchFolders = [workspaceRoot];
-
-defaultConfig.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
-
-defaultConfig.resolver.disableHierarchicalLookup = true;
+const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
