7b8b51f66894e7a5266c3df1afc8813f25aae202
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-13 08:01:51 -0700

    added support for monorepo in metro.config to address bug; turns out was just a type :( committing before revert

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,7 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExt.push('cjs');
+const workspaceRoot = __dirname;
+const projectRoot = path.resolve(workspaceRoot, '../');
+console.log('Project and Workspace roots:', projectRoot, workspaceRoot);
+
+const defaultConfig = getDefaultConfig(projectRoot);
+
+defaultConfig.watchFolders = [workspaceRoot];
+
+defaultConfig.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+defaultConfig.resolver.disableHierarchicalLookup = true;
+defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
