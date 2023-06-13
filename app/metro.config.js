d963347ce58938bd80a0e6d531162dd205d4472c
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-12 21:47:54 -0700

    finished config for firebase js sdk

---
+++ app/metro.config.js
@@ -0,0 +1,7 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.assetExt.push('cjs');
+
+module.exports = defaultConfig;
