101f002cb09f83d504acfa1ba81acf2dcb3534fb
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-13 17:22:07 -0700

    signup successfully implemented with firebase; connecting to firebase auth console

--- app/screens/authentication/Signup.jsx
+++ app/screens/authentication/Signup.jsx
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  View, Text, StyleSheet, TextInput, Button, Alert,
+  View, StyleSheet,
 } from 'react-native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-import { LoginButton, LoginInput } from './components';
+import { LoginButton, LoginInput, LoginError } from './components';
+import { auth } from '../../firebaseConfig';
 
-function Signup() {
+function Signup({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordVis, setPasswordVis] = useState(true);
+  const [rightIcon, setRightIcon] = useState('eye');
+  const [signupError, setSignupError] = useState('');
+
+  const handlePasswordVis = () => {
+    if (rightIcon === 'eye') {
+      setRightIcon('eye-off');
+      setPasswordVis(!passwordVis);
+    } else if (rightIcon === 'eye-off') {
+      setRightIcon('eye');
+      setPasswordVis(!passwordVis);
+    }
+  };
+
+  const handleSignup = async () => {
+    try {
+      if (email !== '' && password !== '') {
+        await createUserWithEmailAndPassword(auth, email, password);
+      }
+    } catch (err) {
+      setSignupError(err.message);
+      console.log(err);
+    }
+  };
   return (
     <View>
       <LoginInput
         placeholder="Email"
         keyboardType="email-address"
         textContentType="emailAddress"
+        leftIcon="email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        autoCapitalize="none"
       />
       <LoginInput
         placeholder="Password"
         autoCorrect={false}
-        secureTextEntry
+        secureTextEntry={passwordVis}
         textContentType="password"
+        leftIcon="lock"
+        rightIcon={rightIcon}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        handlePasswordVis={handlePasswordVis}
+        autoCapitalize="none"
       />
-      <LoginButton title="Sign up" onPress={() => Alert.alert('Signed up!')} />
+      {signupError ? <LoginError error={signupError} visible /> : null}
+      <LoginButton title="Sign up" onPress={handleSignup} />
     </View>
   );
 }
