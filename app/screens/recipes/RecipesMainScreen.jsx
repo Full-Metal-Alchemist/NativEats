b6bfeb9dd3cb613155404d87d1631f18c4fcf9e9
Blake Hansen <b.hansen9264@gmail.com>
2023-06-14 09:42:18 -0700

    Adding for pull

--- app/screens/recipes/RecipesMainScreen.jsx
+++ app/screens/recipes/RecipesMainScreen.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   Text, StyleSheet, View, Button, TouchableOpacity, FlatList, SafeAreaView, Dimensions,
 } from 'react-native';
@@ -12,8 +13,8 @@ const styles = StyleSheet.create({
   },
   touchable: {
     width: 300,
-    borderWidth: 2,
-    borderColor: '#20232a',
+    // borderWidth: 2,
+    // borderColor: '#20232a',
     borderRadius: 6,
     backgroundColor: '#55d6bf',
     color: '#20232a',
@@ -34,24 +35,29 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
 });
+const mock = [{
+  id: '1',
+  name: 'Chicken Katsu',
+  cuisine: ['Japanese'],
+  steps: 'Season the chicken breasts on both sides with salt and pepper',
+  photos: ['https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'],
+  createdAt: '2001-02-16 20:38:40',
+},
+{
+  id: '2',
+  name: 'Indian Chicken Curry',
+  cuisine: ['Indian', 'Middle-Eastern'],
+  steps: 'Sprinkle the chicken breasts with 2 teaspoons salt. Heat oil in a large skillet over high heat',
+  photos: ['https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'],
+  createdAt: '2020-02-11 15:44:17',
+}];
 
 function RecipesMain({ navigation }) {
-  const mock = [{
-    id: '1',
-    name: 'Chicken Katsu',
-    cuisine: ['Japanese'],
-    steps: 'Season the chicken breasts on both sides with salt and pepper',
-    photos: ['https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'],
-    createdAt: '2001-02-16 20:38:40',
-  },
-  {
-    id: '2',
-    name: 'Indian Chicken Curry',
-    cuisine: ['Indian', 'Middle-Eastern'],
-    steps: 'Sprinkle the chicken breasts with 2 teaspoons salt. Heat oil in a large skillet over high heat',
-    photos: ['https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'],
-    createdAt: '2020-02-11 15:44:17',
-  }];
+  useEffect(() => {
+    axios.get('/api/v1/recipes')
+      .then((res) => console.log('This is response Line 58: ', res))
+      .catch((err) => console.log('This is error Line 59: ', err));
+  }, []);
   return (
     <SafeAreaView style={styles.list}>
       <View style={styles.container}>
