563e67c84a46c86aba2c47bbd434f49a625f8b5e
Frank <mirandofrank@gmail.com>
2023-06-16 01:10:51 -0700

    Can now grab cuisine data in Cultural.jsx

--- app/screens/cultural/Cultural.jsx
+++ app/screens/cultural/Cultural.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import {
   Text, StyleSheet, View, SafeAreaView, ScrollView,
@@ -25,17 +26,42 @@ const styles = StyleSheet.create({
 
 function CulturalInsights() {
   // Temporary state to hold placeholder cuisine chosen
-  const [currentCuisine, setCurrentCuisine] = useState('Italian');
+  const tempId = 1;
+  // id: 1 is Italian
+  // TODO: grab current cuisine from nathan
+  const [currentCuisine, setCurrentCuisine] = useState({});
+
+  useEffect(() => {
+    // TODO: grab data for current cuisine
+    axios.get('http://localhost:8080/api/v1/cuisines')
+      .then(({ data }) => {
+        // console.log('Cultural: grabbing cuisines', data);
+        // setCurrentCuisine(data.filter((c) => c.id === tempId));
+        data.forEach((cuisine) => {
+          if (cuisine.id === tempId) {
+            setCurrentCuisine(cuisine);
+          }
+        });
+      })
+      .catch((err) => {
+        console.log('Couldn\'t grab cuisines data...', err);
+      });
+  }, []);
+
   return (
     <SafeAreaView>
       <ScrollView>
         <Text style={styles.title}>
           Cultural Insights About
           {' '}
-          {currentCuisine}
+          {currentCuisine.name}
           {' '}
           Food
         </Text>
+
+        <View>
+
+        </View>
 
       </ScrollView>
     </SafeAreaView>
