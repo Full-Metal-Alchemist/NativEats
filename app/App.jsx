3d576e2daf4170bc7ac2d20fc6fc0dd1d73b2d79
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-16 13:26:05 -0700

    @auth (#47)
    * set up eas and expo dev-client. installed firebase app and auth
    
    * adding package-lock
    
    * configured eas.json and app.json to work with react native firebase modules
    
    * Revert "adding package-lock"
    
    This reverts commit 341d61a8fb47be7e34ba389b46b275e37069c7bf.
    
    * Revert "Functionality for Recipe (#12)"
    
    This reverts commit 3bc1c756ea017da66a7e30b003f78cbc17168025.
    
    * Revert "set up eas and expo dev-client. installed firebase app and auth"
    
    This reverts commit 6af89b536e6cd420720a252b24af62c383e53322.
    
    * deleted eas.json; no longer necessary to use EAS for firebase auth
    
    * finished config for firebase js sdk
    
    * exported app and auth constants
    
    * added support for monorepo in metro.config to address bug; turns out was just a type :( committing before revert
    
    * reverted metro.config to config recommended by using firebase expo docs
    
    * started login screen component
    
    * created basic pages for sign in, sign out, forgot password, and mock home screen
    
    * implemented LoginButton and LoginInput components for Login Screens
    
    * added IconButton and LoginError components
    
    * added package-lock changes
    
    * Login Screen implemented with firebase
    
    * signup successfully implemented with firebase; connecting to firebase auth console
    
    * auth implemented
    
    * solved circular dependency issues with useContext
    
    * updated colors for home screens
    
    * added pop animation to signout
    
    * minor styling; fixed bug with showPassword
    
    * added first name, last name, and username input fields
    
    * fixed issue with logout animation
    
    * added nav to mockHomeScreen
    
    * installed morgan for express logging
    
    * added test request button to mockhomescreen to troubleshoot axios issues
    
    * implemented firebase auth middleware
    
    * auth middleware now working
    
    * updated firebase auth middleware to get user information
    
    * updated handleSignup to update user profile. changed MockHomeScreen test req to work with auth
    
    * committing package-lock updates
    
    * updated createUser controller to properly insert new user
    
    * updated createUser catch block
    
    * user post request working
    
    * fished get route for users endpoint
    
    * implemented get request to set puid
    
    * reinstalled dependencies
    
    * styling on login. added logos
    
    * reinstalled deps

--- app/App.jsx
+++ app/App.jsx
@@ -68,6 +68,7 @@ export default function App() {
               <Stack.Screen name="Restaurants" component={HomePage} />
               <Stack.Screen name="RecipesMain" component={RecipesMain} />
               <Stack.Screen name="MockHomeScreen" component={MockHomeScreen} />
+              <Stack.Screen name="RecipesMain" component={RecipesMain} />
               <Stack.Screen name="RecipesSingle" component={RecipesSingle} />
               <Stack.Screen name="FoodieTour" component={FoodieTour} />
               <Stack.Screen name="SettingsPage" component={SettingsPage} />
