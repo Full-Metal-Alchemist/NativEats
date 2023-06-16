7cae873437921d09a8939c381687c90ce35c7bad
yluo12 <yantingluo98@gmail.com>
2023-06-16 09:20:19 -0700

    updated import statement in homepage and bookmark index

--- app/screens/HomePage/index.jsx
+++ app/screens/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View, Text, StyleSheet, FlatList, Image, Button, Dimensions, SafeAreaView,
 } from 'react-native';
